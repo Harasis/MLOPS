7a53ba61c36d0ff1beda0ffdc2ff8a781708f54c
Harasis Singh <51106510+Harasis@users.noreply.github.com>
2020-07-19 02:52:08 +0530

    Add files via upload

---
+++ Ml/mymenu.py
@@ -0,0 +1,6 @@
+from sklearn.externals import joblib
+salary_model = joblib.load("Salary_model.pk1")
+
+
+print("\t\t\t Welcome To My Machine Learning Menu")
+print("\t\t\t--------------------------------------")
